b64a9e1b3166840f3f03033a9a1829019bc3b5b5
DMX101 <101dmx@gmail.com>
2015-10-21 23:29:51 +0300

    Commit 2

---
+++ Lesson_19/src/com/vynipox/glm/Test.java
@@ -0,0 +1,21 @@
+package com.vynipox.glm;
+
+/**
+ * Created by USER on 21.10.2015.
+ */
+public class Test {
+
+    public int summ(int a, int b) {
+
+        return a+b;
+
+    }
+
+
+    public void test() {
+
+        System.out.println("New change");
+
+    }
+
+}
